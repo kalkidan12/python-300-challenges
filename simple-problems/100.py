81dd638b9c0093309c488cb80d12c8aee5620ce7
kalkidan12 <kalget12@gmail.com>
2023-01-11 01:28:47 +0300

    Q.100

--- simple-problems/100.py
+++ simple-problems/100.py
@@ -1,4 +1,4 @@
-#100. Write a python program to add number from list that are greater than 5 and lessthan 10.
+#Q.100. Write a python program to add number from list that are greater than 5 and lessthan 10.
 
 numbers = [1,2,3,4,5,6,7,8,11,22,33,55]
 

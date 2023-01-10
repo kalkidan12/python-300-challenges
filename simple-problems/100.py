6d49b8d7a11a6f789d32ae47f5f1b129b47f7b29
kalkidan12 <kalget12@gmail.com>
2023-01-11 00:46:02 +0300

    100

---
+++ simple-problems/100.py
@@ -0,0 +1,19 @@
+#100. Write a python program to add number from list that are greater than 5 and lessthan 10.
+
+numbers = [1,2,3,4,5,6,7,8,11,22,33,55]
+
+sum = 0
+for i in numbers:
+    if(i>5 and i<10):
+        sum +=i
+print(sum)
+
+
+#challenge 
+# write a program to add even number from list
+evenTotal = 0
+for i in numbers:
+    if(i%2 == 0):
+        evenTotal +=i
+print(evenTotal)
+        

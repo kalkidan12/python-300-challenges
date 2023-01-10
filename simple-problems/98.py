c1140e94af28eecf9adb047cbe9886203adba744
kalkidan12 <kalget12@gmail.com>
2023-01-11 01:28:19 +0300

    Q.98

---
+++ simple-problems/98.py
@@ -0,0 +1,12 @@
+#Q.98. Write a python program to find the middle element of list items.
+import math
+
+
+my_list = [1,2,3,4,5,6,7,8,9]
+middle = int(len(my_list)/2)
+print(my_list[middle])
+
+
+#challenge
+# Write a python program to find the squre root of middle element of list items.
+print(math.sqrt(my_list[middle]))

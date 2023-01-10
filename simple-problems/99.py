ae8fbe5291e3fc2264d9f500305a3984ec70d0d3
kalkidan12 <kalget12@gmail.com>
2023-01-11 01:07:15 +0300

    99

---
+++ simple-problems/99.py
@@ -0,0 +1,27 @@
+#99. Write a python program to get only uppercase word from user store in a list
+
+upper_list = []
+
+for i in range(10):
+    w = input('write word in upper case' )
+    if (w.isupper()):
+        upper_list.append(w)
+    else:
+        print("error! insert only uppercase word")
+print(upper_list)
+
+
+#challenge
+#99. Write a python program to get only lowercase word from user store in a list
+
+lower_list = []
+
+for i in range(10):
+    w = input('write word in lower case ')
+    if (w.islower()):
+        lower_list.append(w)
+    else:
+        print("error! insert only lowercase word")
+print(lower_list)
+    
+

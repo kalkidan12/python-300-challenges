5d0d6d884bb1d5afd1f8cefe302fc82f755880f6
kalkidan12 <kalget12@gmail.com>
2023-01-11 15:16:42 +0300

    Q.88

---
+++ simple-problems/88.py
@@ -0,0 +1,6 @@
+#Q.88. Write a python program to get a number from user , the system should add auto 
+# increment to that number . half of that number should be incremented.
+
+number  = float(input('Enter any number you want '))
+halfNumber = number/2
+print(number+halfNumber)

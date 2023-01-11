c746f2de349ebdfe6ef9d171694ba58c804c9c99
kalkidan12 <kalget12@gmail.com>
2023-01-11 16:21:13 +0300

    Q.86 & Q.87

---
+++ simple-problems/87.py
@@ -0,0 +1,12 @@
+#Q.88. Write a python program to get a sentence from user , the system should  display 
+# only ten character maximum.
+
+sentence  = str(input('Enter sentence here '))
+
+
+print(sentence[slice(0,10)])
+# or
+print(sentence[0:10], end='')
+
+
+    

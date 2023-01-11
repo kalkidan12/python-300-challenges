17e4740f3dd53754cdd38332ad84cbca1694cad2
kalkidan12 <kalget12@gmail.com>
2023-01-11 10:39:31 +0300

    Q.94

---
+++ simple-problems/94.py
@@ -0,0 +1,7 @@
+#Q.94. Write a python program to find force of man on a object which have mas and acceleration. get mass and acceleration from user 
+# formula F=ma.
+
+m = int(input('Enter the mass '))
+a = int(input('Enter the acceleration '))
+f = m*a
+print(f"force equals to: {f}")

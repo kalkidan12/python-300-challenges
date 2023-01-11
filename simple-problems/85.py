0a223c8815a4b77ca46a2cf44e374d71f41caee4
kalkidan12 <kalget12@gmail.com>
2023-01-11 16:48:30 +0300

    Q.85

---
+++ simple-problems/85.py
@@ -0,0 +1,4 @@
+#Q.85. Write a python program to get name from a list, that ende at 'n.
+my_list = ['kalkidan', 'promise', 'anan', 'kebede']
+
+print([name for name in my_list if name.endswith('n')])
